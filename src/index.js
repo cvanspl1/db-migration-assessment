5a877812e38e5e1459db0add487f647976370c33
Corey Van Splinter <cvanspl1@gmail.com>
2021-01-31 12:30:25 -0500

    Confirmed ability to query both databases

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
 const DB = require('./dbPools.js');
+
+// Attempts to retrieve all data from the original database - calls createOldAccountsTable if successful
+const getAllMigratedAccounts = () => {
+  DB.queryMigrated('SELECT * FROM accounts')
+    .then((res) => {
+      console.log(res.rows);
+    })
+    .catch((err) => {
+      console.log('ERROR: Failed to retrieve data from migrated accounts table - ', err);
+    });
+};
+
+// Attempts to retrieve all data from the original database - calls createOldAccountsTable if successful
+const getAllOriginalAccounts = () => {
+  DB.queryOriginal('SELECT * FROM accounts')
+    .then((res) => {
+      console.log(res.rows);
+    })
+    .catch((err) => {
+      console.log('ERROR: Failed to retrieve data from original accounts table - ', err);
+    });
+};
+
+getAllOriginalAccounts();
+getAllMigratedAccounts();
