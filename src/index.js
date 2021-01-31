b9489c760a69dab934b02796e8d1c26ecf98d0c9
Corey Van Splinter <cvanspl1@gmail.com>
2021-01-31 13:21:57 -0500

    Added code to generate data used for report

--- src/index.js
+++ src/index.js
@@ -1,13 +1,72 @@
-const DB = require('./dbPools.js');
+const db = require('./dbPools.js');
 
-// Attempts to retrieve all data from the original database - calls createOldAccountsTable if successful
-const getAllMigratedAccounts = () => {
-  DB.queryMigrated('SELECT * FROM accounts')
-    .then((res) => {
-      console.log(res.rows);
+// Attempts to remove the old_accounts table from the new database
+const dropOldAccountsTable = () => {
+  DB.queryMigrated('DROP TABLE IF EXISTS old_accounts')
+    .then(() => {
+      console.log('EXECUTION COMPLETE - closing DB connections...');
     })
     .catch((err) => {
-      console.log('ERROR: Failed to retrieve data from migrated accounts table - ', err);
+      console.log('ERROR: Failed to remove old_accounts table from new database - ', err);
+    });
+};
+
+// Attempts to create the error report - calls dropOldAccountsTable regardless of success status
+const createReport = () => {
+  DB.queryMigrated(
+    `SELECT 
+      old_accounts.id AS pre_migration_id,
+      accounts.id AS post_migration_id, 
+      (old_accounts.name <> accounts.name) AS name_corrupted,
+      (old_accounts.email <> accounts.email) AS email_corrupted, 
+      (old_accounts.id IS NULL) AS added_after_migration,
+      (accounts.id IS NULL) AS lost_during_migration
+    FROM accounts
+    FULL JOIN old_accounts
+      ON accounts.id = old_accounts.id`
+  )
+    .then((res) => {
+      console.log(res.rows);
+      dropOldAccountsTable();
+    })
+    .catch((err) => {
+      console.log('ERROR: Unable to generate report - ', err);
+      dropOldAccountsTable();
+    });
+};
+
+// Recursively calls itelf until all rows are inserted into the new table, or an error occurs
+// Calls createReport when all rows have been inserted, calls dropOldAccountsTable on error
+const insertOldAccountRow = (rows, idx = 0) => {
+  if (idx === rows.length) {
+    createReport();
+  } else {
+    DB.queryMigrated('INSERT INTO old_accounts(id, name, email) VALUES($1, $2, $3)', [
+      rows[idx].id,
+      rows[idx].name,
+      rows[idx].email,
+    ])
+      .then(() => {
+        insertOldAccountRow(rows, idx + 1);
+      })
+      .catch((err) => {
+        console.log(
+          'ERROR: Unable to copy original account data into the old_accounts table - ',
+          err
+        );
+        dropOldAccountsTable();
+      });
+  }
+};
+
+// Attempts to create an old_accounts table in the new DB - calls insertOldAccountRow if successful
+const createOldAccountsTable = (rows) => {
+  DB.queryMigrated('CREATE TABLE old_accounts(id TEXT, name TEXT, email TEXT)')
+    .then(() => {
+      insertOldAccountRow(rows);
+    })
+    .catch((err) => {
+      console.log('ERROR: Failed to create old_accounts table in new database - ', err);
     });
 };
 
@@ -15,7 +74,7 @@ const getAllMigratedAccounts = () => {
 const getAllOriginalAccounts = () => {
   DB.queryOriginal('SELECT * FROM accounts')
     .then((res) => {
-      console.log(res.rows);
+      createOldAccountsTable(res.rows);
     })
     .catch((err) => {
       console.log('ERROR: Failed to retrieve data from original accounts table - ', err);
@@ -23,4 +82,3 @@ const getAllOriginalAccounts = () => {
 };
 
 getAllOriginalAccounts();
-getAllMigratedAccounts();
