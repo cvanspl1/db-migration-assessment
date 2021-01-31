bc38014056d37b107e32f091c39bcf5dc08a621e
Corey Van Splinter <cvanspl1@gmail.com>
2021-01-31 14:24:03 -0500

    Added generation of csv report and robust logs

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
+const csv = require('fast-csv');
+const ws = require('fs').createWriteStream('report.csv');
 const db = require('./dbPools.js');
 
 // Attempts to remove the old_accounts table from the new database
 const dropOldAccountsTable = () => {
-  DB.queryMigrated('DROP TABLE IF EXISTS old_accounts')
+  console.log('Attempting to remove old_accounts table from new database...');
+  db.queryMigrated('DROP TABLE IF EXISTS old_accounts')
     .then(() => {
-      console.log('EXECUTION COMPLETE - closing DB connections...');
+      console.log('SUCCESS: old_accounts table removed from new database');
+      console.log('EXECUTION COMPLETE - closing db connections...');
     })
     .catch((err) => {
       console.log('ERROR: Failed to remove old_accounts table from new database - ', err);
@@ -13,7 +17,8 @@ const dropOldAccountsTable = () => {
 
 // Attempts to create the error report - calls dropOldAccountsTable regardless of success status
 const createReport = () => {
-  DB.queryMigrated(
+  console.log('Attempting to generate report...');
+  db.queryMigrated(
     `SELECT 
       old_accounts.id AS pre_migration_id,
       accounts.id AS post_migration_id, 
@@ -26,8 +31,13 @@ const createReport = () => {
       ON accounts.id = old_accounts.id`
   )
     .then((res) => {
-      console.log(res.rows);
-      dropOldAccountsTable();
+      const jsonData = JSON.parse(JSON.stringify(res.rows));
+      csv.write(jsonData, { headers: true })
+        .on('finish', () => {
+          console.log(`SUCCESS: Account data exported to report.csv`);
+          dropOldAccountsTable();
+        })
+        .pipe(ws);
     })
     .catch((err) => {
       console.log('ERROR: Unable to generate report - ', err);
@@ -39,9 +49,10 @@ const createReport = () => {
 // Calls createReport when all rows have been inserted, calls dropOldAccountsTable on error
 const insertOldAccountRow = (rows, idx = 0) => {
   if (idx === rows.length) {
+    console.log('SUCCESS: ALL original account data was copied into the old_accounts table');
     createReport();
   } else {
-    DB.queryMigrated('INSERT INTO old_accounts(id, name, email) VALUES($1, $2, $3)', [
+    db.queryMigrated('INSERT INTO old_accounts(id, name, email) VALUES($1, $2, $3)', [
       rows[idx].id,
       rows[idx].name,
       rows[idx].email,
@@ -59,10 +70,15 @@ const insertOldAccountRow = (rows, idx = 0) => {
   }
 };
 
-// Attempts to create an old_accounts table in the new DB - calls insertOldAccountRow if successful
+// Attempts to create an old_accounts table in the new db - calls insertOldAccountRow if successful
 const createOldAccountsTable = (rows) => {
-  DB.queryMigrated('CREATE TABLE old_accounts(id TEXT, name TEXT, email TEXT)')
+  console.log('Attempting to create old_accounts table in new database...');
+  db.queryMigrated('CREATE TABLE old_accounts(id TEXT, name TEXT, email TEXT)')
     .then(() => {
+      console.log('SUCCESS: old_accounts table created in new database');
+      console.log(
+        'Attempting to copy original account data into the old_accounts table... (this may take a several minutes)'
+      );
       insertOldAccountRow(rows);
     })
     .catch((err) => {
@@ -72,8 +88,10 @@ const createOldAccountsTable = (rows) => {
 
 // Attempts to retrieve all data from the original database - calls createOldAccountsTable if successful
 const getAllOriginalAccounts = () => {
-  DB.queryOriginal('SELECT * FROM accounts')
+  console.log('Attempting to retrieve data from original accounts table...');
+  db.queryOriginal('SELECT * FROM accounts')
     .then((res) => {
+      console.log('SUCCESS: Data retrieved from original accounts table');
       createOldAccountsTable(res.rows);
     })
     .catch((err) => {
